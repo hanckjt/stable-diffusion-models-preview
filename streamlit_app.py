d7e4cbc33b30fd4c054ac6bea706c9c380d94d71
Hank <hanckjt@gmail.com>
2023-02-13 19:17:01 +0800

    add vscode config

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+app_main_title = "Stable Diffusion Models Previewer"
+
+def main():
+    st.set_page_config(page_title=app_main_title, page_icon="🚀", layout="wide", initial_sidebar_state="auto")
+
+    
+if __name__ == "__main__":
+    main()
